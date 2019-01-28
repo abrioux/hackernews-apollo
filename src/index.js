f8ffade524134d56ef38ee724b62ea899f65fce6
Alex Brioux <abrioux25@gmail.com>
2019-01-27 19:10:33 -0800

    Routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import { register as registerServiceWorker} from './serviceWorker'
+import { register as registerServiceWorker } from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
@@ -18,9 +19,12 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
+
 registerServiceWorker()
